ee84b1eb0d197d8cee7f4b555b7a10f386b2693c
oskarsvensson <oskarsvensson@localhost>
2025-11-28 13:57:27 +0100

    First commit!

--- src/dungeongame/Dungeon.java
+++ src/dungeongame/Dungeon.java
@@ -7,6 +7,8 @@ public class Dungeon {
     private Room room1 = new Room("Detta är rum 1");
     private Room room2 = new Room("Detta är rum 2");
     private Room room3 = new Room("Detta är rum 3");
+    private Room room4 = new Room("Detta är rum 4");
+
             
     public void playGame() {
         currentRoom = room1;
